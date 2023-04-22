e7cb359d772931dcfdf40eef66ffa0db6a9d00bb
sykoe <953678+sykoe@users.noreply.github.com>
2023-04-22 19:28:55 +0200

    Update chat_stalker.user.js

--- chat_stalker.user.js
+++ chat_stalker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat Stalker
-// @namespace    sykoe.chatstalker
-// @version      1.5.1
+// @namespace    sykoe.chatstalkerbeta
+// @version      1.6.1
 // @description  Notifies when a user post in global or trade chat (forked from Hardy[2131687]). Does NOT work when global/trade chat is disabled via torntools.
 // @author       Sykoe[2734951]
 // @match        https://www.torn.com/*
