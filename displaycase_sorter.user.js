203cc1a0a951be1e8970cc4a45a7a3278d60891a
sykoe <953678+sykoe@users.noreply.github.com>
2023-05-10 19:16:44 +0200

    Create displaycase_sorter.user.js

---
+++ displaycase_sorter.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Sort items in display case manage view
+// @namespace    sykoe.displaycase.sorter
+// @version      0.1
+// @description  Sort display case with one button
+// @author       Sykoe[2734951]
+// @match        https://www.torn.com/displaycase*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    window.addEventListener(
+        "load",
+        function () {
+            //DOM has loaded
+            setTimeout(() => addButton(), 200);
+        },
+        false
+    );
+
+    function addButton() {
+        const button = `<button id="sortItems" style="color: var(--default-blue-color); cursor: pointer; margin-right: 0;">Sort!</button>`;
+        $('#skip-to-content').append(button);
+
+        $('#sortItems').on('click', (e) => {
+            sortItems(e);
+        });
+    };
+
+    function sortItems(e) {
+        const items = Array.from(document.querySelectorAll("ul.dc-list > li"));
+        var $list = $('ul.dc-list');
+        var $listLi = $('ul.dc-list > li');
+        $listLi.sort(function (a, b) {
+            var textA = a.attributes['aria-label'].textContent.toLowerCase();
+            var textB = b.attributes['aria-label'].textContent.toLowerCase();
+            return (textA > textB) ? 1 : 0;
+        });
+        $.each($listLi, function (index, row) {
+            $list.append(row);
+        });
+        e.preventDefault();
+    }
+})();
